1373f71ce18975db041f317d41d3cd1ed6d9d8ba
XRed_CubeX <XRed_CubeX@hotmail.com>
2021-03-28 09:49:51 +0200

    Fix HIDL manifest

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,4 +11,4 @@ TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_PATH)/include
 -include $(DEVICE_PATH)/board/*.mk
 
 # Manifest
-DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/manifest.xml
