237b9f94f6ad57b7832e5f59afd6c076c195b211
R0rt1z2 <rogerortizleal@gmail.com>
2021-03-28 10:35:59 +0200

    libshims: gui: include string header

--- mtk/libshims/gui/mtk_gui.cpp
+++ mtk/libshims/gui/mtk_gui.cpp
@@ -1,5 +1,6 @@
 #include <ui/GraphicBufferMapper.h>
 #include <ui/Rect.h>
+#include <string>
 
 extern "C" {
     void _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPv(buffer_handle_t, uint32_t, const android::Rect&, void**);
