652b360544d2aca3fafdb53f39c6459ee165e90a
XRedCubeX <57260282+XRedCubeX@users.noreply.github.com>
2021-03-28 12:32:32 +0200

    Rebrand

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
 #used to add device to CM's lunch
-add_lunch_combo cm_m2note-user
-add_lunch_combo cm_m2note-userdebug
-add_lunch_combo cm_m2note-eng
+add_lunch_combo lineage_m2note-user
+add_lunch_combo lineage_m2note-userdebug
+add_lunch_combo lineage_m2note-eng
