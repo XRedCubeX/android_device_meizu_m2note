b85721e5e23338b5ae46f1b97f140d309c98746c
xcore995 <arman.p95@gmail.com>
2021-03-28 09:47:03 +0200

    use legacy camera hal

--- product/packages.mk
+++ product/packages.mk
@@ -90,7 +90,4 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    camera.device@1.0-impl \
-    camera.device@3.2-impl \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service
+    android.hardware.camera.provider@2.4-impl-legacy
